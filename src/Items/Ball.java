20793d2b53b4fa602f8db50aa623900c4302a6e9
Mahdi Barani <mahdi.mb.1384@gmail.com>
2024-03-11 08:07:26 -0700

    Creating A Collision Logic For Ball

--- src/Items/Ball.java
+++ src/Items/Ball.java
@@ -18,6 +18,19 @@ public class Ball extends OIG{
         return margin;
     }
 
+    boolean collision(OIG a){
+        ArrayList<Point> ballMargin = getMargin();
+        ArrayList<Point> aMargin = a.getMargin();
+        for (int i = 0 ;i < ballMargin.size() ;i++){
+            for (int j = 0 ;j < aMargin.size() ;j++){
+                if (ballMargin.get(i).getX() == aMargin.get(j).getX() && ballMargin.get(i).getY() == aMargin.get(j).getY()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 
     @Override
     public void draw(Graphics g) {
